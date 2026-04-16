5336833acf82340e5a8d7da279120e7869c99ea2
nisatosun <nisa.tosun@ogr.sakarya.edu.tr>
2026-04-16 20:09:36 +0300

    chore: initialize Spring Boot backend and health endpoint

---
+++ src/test/java/com/nisa/itsm/ItsmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisa.itsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
