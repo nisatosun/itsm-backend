748cfc5d3bdd3b77f11597c05767819127d3af1a
nisatosun <nisa.tosun@ogr.sakarya.edu.tr>
2026-04-21 02:23:22 +0300

    feat: initial ITSM backend setup with user module

---
+++ src/main/java/com/nisa/itsm/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.nisa.itsm.user.repository;
+
+import com.nisa.itsm.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
